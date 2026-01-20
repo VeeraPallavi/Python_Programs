59d8ebb9bcd1f55805a3560bb2549869c1bfaea9
VeeraPallavi <pallavinandimandalam@gmail.com>
2026-01-20 16:45:47 +0530

    Implemented Merge Sort algorithm

---
+++ Sorting/merge_sort.py
@@ -0,0 +1,36 @@
+def merge_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+
+    #Divide
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i= j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+        
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+arr = [6, 9, 4, 2, 7, 8, 1]
+print(merge_sort(arr))
